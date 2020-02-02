148f785820b99463eb5f1dcedfc45af3b3b58718
Ivan Rossi <ivanross.me@gmail.com>
2020-02-02 17:16:32 +0100

    🐞 add missing types in index.d.ts

--- index.test-d.tsx
+++ index.test-d.tsx
@@ -1,13 +1,18 @@
-import * as React from 'react';
-import MultiSelect, {Item, IndicatorProps, ItemProps, CheckBoxProps} from '.';
+import * as React from "react";
+import MultiSelect, {
+	ListedItem,
+	IndicatorProps,
+	ItemProps,
+	CheckBoxProps
+} from ".";
 
-const items: Item[] = [
-	{label: 'label 1', value: 'label 1'},
-	{label: 'label 2', value: 'label 2'}
+const items: ListedItem[] = [
+	{ label: "label 1", value: "label 1" },
+	{ label: "label 2", value: "label 2" }
 ];
-const keyedItems: Item[] = [
-	{label: 'label 1', value: 'label 1', key: 1},
-	{label: 'label 2', value: 'label 2', key: 2}
+const keyedItems: ListedItem[] = [
+	{ label: "label 1", value: "label 1", key: 1 },
+	{ label: "label 2", value: "label 2", key: 2 }
 ];
 
 const plain = () => <MultiSelect />;
@@ -16,24 +21,24 @@ const withProps = () => <MultiSelect focus={false} limit={10} />;
 const selectWithItems = () => <MultiSelect items={items} />;
 const selectWithKeyedItems = () => <MultiSelect items={keyedItems} />;
 
-const onSelect = (item: Item) => console.log(item);
+const onSelect = (item: ListedItem) => console.log(item);
 const withSelectHandler = () => <MultiSelect onSelect={onSelect} />;
 
-const onUnselect = (item: Item) => console.log(item);
+const onUnselect = (item: ListedItem) => console.log(item);
 const withUnselectHandler = () => <MultiSelect onUnselect={onUnselect} />;
 
-const onSubmit = (items: Item[]) => console.log(items);
+const onSubmit = (items: ListedItem[]) => console.log(items);
 const withSubmitHandler = () => <MultiSelect onSubmit={onSubmit} />;
 
-const CustomIndikator: React.FC<IndicatorProps> = ({isHighlighted}) => (
-	<div>{isHighlighted ? '✓' : ''}</div>
+const CustomIndikator: React.FC<IndicatorProps> = ({ isHighlighted }) => (
+	<div>{isHighlighted ? "✓" : ""}</div>
 );
-const CustomCheckBox: React.FC<CheckBoxProps> = ({isSelected}) => (
-	<div>{isSelected ? '✓' : ''}</div>
+const CustomCheckBox: React.FC<CheckBoxProps> = ({ isSelected }) => (
+	<div>{isSelected ? "✓" : ""}</div>
 );
-const CustomItemComponent: React.FC<ItemProps> = ({isHighlighted, label}) => (
+const CustomItemComponent: React.FC<ItemProps> = ({ isHighlighted, label }) => (
 	<div>
-		{isHighlighted ? '✓' : ''} {label}
+		{isHighlighted ? "✓" : ""} {label}
 	</div>
 );
 const overrideComponents = () => (
